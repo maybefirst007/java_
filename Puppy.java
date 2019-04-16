38df643ac218a254d8c404a3c012ef423d1bb470
maybefirst007 <48748683+maybefirst007@users.noreply.github.com>
2019-04-16 13:15:12 +0800

    Add files via upload

---
+++ Puppy.java
@@ -0,0 +1,29 @@
+/*public class HalloWorld {*/
+/*	public static void main(String[] args){*/
+/*		System.out.println("Hello World");*/
+/*	}*/
+/*}*/
+public class Puppy{
+	int puppyAge;
+	public Puppy(String name){
+		//gouzaoqi : name
+		System.out.println("dog : " + name);
+	}
+
+	public void setAge( int age ){
+		puppyAge = age;
+	}
+	public int getAge(){
+		System.out.println("dog : " + puppyAge);
+		return puppyAge;
+	}
+
+	public static void main(String[] args){
+		/*object*/
+		Puppy myPuppy = new Puppy("tommy");
+		/*fangfa*/
+		myPuppy.setAge( 2 );
+		myPuppy.getAge( );
+		System.out.println("bianliangzhi : " + myPuppy.puppyAge );
+	}
+}
